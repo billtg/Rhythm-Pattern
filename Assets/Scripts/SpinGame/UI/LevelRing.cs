56061a78002f5cad0a1cb19cbf0960db02172d29
Graham Tattersall <billtg@users.noreply.github.com>
2019-05-02 15:47:31 -0230

    0.21 - Hiphop 4
    Last hiphop song done
    
    Aded complete rings to the level select menu
    
    Still more stuff to do, but game mostly done.
    Fix Dance2 and hiphop 1

---
+++ Assets/Scripts/SpinGame/UI/LevelRing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelRing : MonoBehaviour
+{
+    public SongType songtype;
+    private float hiphopScale = 1.75f;
+
+    private void Awake()
+    {
+        int score;
+        switch (songtype)
+        {
+            case SongType.Dance:
+                score = PlayerPrefs.GetInt("Dance", 0);
+                if (score >= 4)
+                    this.transform.localScale = Vector3.one;
+                break;
+            case SongType.Hiphop:
+                score = PlayerPrefs.GetInt("Hiphop", 0);
+                if (score >= 4)
+                    this.transform.localScale = new Vector3(hiphopScale,hiphopScale,1);
+                break;
+        }
+    }
+}
